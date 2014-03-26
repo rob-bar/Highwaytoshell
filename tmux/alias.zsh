a8d7625ead68c86715bfc67d25190d603aa146f8
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-03-26 16:14:07 +0100

    more tmux aliases

--- tmux/alias.zsh
+++ tmux/alias.zsh
@@ -12,6 +12,10 @@ alias tmd='tmux detach'
 # PANES
 alias tmsph='tmux split-window -h -p'
 alias tmspv='tmux split-window -v -p'
+alias tsp2='tmux split-window -h -p 20'
+alias tsp3='tmux split-window -h -p 30'
+alias tsp5='tmux split-window -h -p 50'
+alias tsp8='tmux split-window -h -p 80'
 
 # SWITCH PANES
 alias tmsp='tmux select-pane'
