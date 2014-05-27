a6ac0686766553e6f9dd545e74f368b04c4b28ae
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-05-27 08:59:55 +0200

    drush and cleanup

--- drush/alias.zsh
+++ drush/alias.zsh
@@ -10,3 +10,6 @@ alias ds='d status'
 alias dup='drush generate-content'
 alias dpass='d upwd admin --password=admin'
 alias dclear='drush updb; drush fra -y; drush cc all;'
+
+alias d0='drush vset --yes preprocess_css 0;drush vset --yes preprocess_js 0;drush vset --yes cache 0;drush vset --yes block_cache 0;drush vset --yes page_compression 0;drush vset --yes error_level 0'
+alias d1='drush vset --yes preprocess_css 1;drush vset --yes preprocess_js 1;drush vset --yes cache 1;drush vset --yes block_cache 1;drush vset --yes page_compression 1;drush vset --yes error_level 1'
